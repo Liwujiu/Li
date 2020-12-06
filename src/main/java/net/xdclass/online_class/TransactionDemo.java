827ba9f7a0a0d0cd2d918b9ad9e8487fdca26a5e
Liwujiu <853867239@qq.com>
2020-12-06 22:33:46 +0800

    数据库事务

---
+++ src/main/java/net/xdclass/online_class/TransactionDemo.java
@@ -0,0 +1,46 @@
+package net.xdclass.online_class;
+
+import net.xdclass.online_class.dao.VideoMapper;
+import net.xdclass.online_class.dao.VideoOrderMapper;
+import net.xdclass.online_class.domain.Video;
+import net.xdclass.online_class.domain.VideoOrder;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+public class TransactionDemo {
+
+
+    public static void main(String[] args) throws IOException {
+
+        String resouce = "config/mybatis-config.xml";
+
+        //读取配置文件
+        InputStream inputStream = Resources.getResourceAsStream(resouce);
+
+        //构建Session工厂
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        //获取Session
+        SqlSession sqlSession = sqlSessionFactory.openSession(false);
+        try {
+            VideoMapper videoMapper = sqlSession.getMapper(VideoMapper.class);
+            Video video1 = new Video();
+            video1.setTitle("微服务架构");
+            videoMapper.add(video1);
+            int i = 1 / 0;
+            sqlSession.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            sqlSession.rollback();
+        }
+        sqlSession.close();
+
+    }
+
+}
